d1b298a41091699a7884c88af8866b603a34d809
Nishanth <63568590+iamnishanth@users.noreply.github.com>
2021-01-24 10:04:50 +0530

    Day 13 Task - CallBack Hell uploaded

---
+++ Day-13-Tasks/script.js
@@ -0,0 +1,39 @@
+setTimeout(() => {
+    console.log(10);
+    setTimeout(() => {
+        console.log(9);
+        setTimeout(() => {
+            console.log(8);
+            setTimeout(() => {
+                console.log(7);
+                setTimeout(() => {
+                    console.log(6);
+                    setTimeout(() => {
+                        console.log(5);
+                        setTimeout(() => {
+                            console.log(4);
+                            setTimeout(() => {
+                                console.log(3);
+                                setTimeout(() => {
+                                    console.log(2);
+                                    setTimeout(() => {
+                                        console.log(1);
+                                        setTimeout(() => {
+                                            console.log(0);
+                                            setTimeout(() => {
+                                                console.log("Happy Independance Day!");
+                                                setTimeout(() => {
+                                                    console.log("Refresh to see it again!");
+                                                }, 4000)
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    },1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    },1000)
+},1000)
