3950f609ad93380baa6d2bc30a7fd68ff0f2a958
Nishanth <63568590+iamnishanth@users.noreply.github.com>
2021-03-29 05:37:27 +0530

    Quizzler - React

---
+++ Quizzler/src/pages/Home.js
@@ -0,0 +1,39 @@
+import { Container, Row, Col, Card } from "reactstrap";
+import { useHistory } from "react-router-dom";
+
+import "./Home.css";
+
+const Home = (props) => {
+  const history = useHistory();
+  const handleClick = (item) => {
+    props.handleTopicSelect(item);
+    history.push("/topic");
+  };
+  return (
+    <Container>
+      <div className="m-5">
+        <h1>Quizzler</h1>
+      </div>
+      <Container>
+        <Row>
+          {props.data.map((item) => {
+            return (
+              <Col sm="12" md="4" key={item.id}>
+                <Card
+                  className="card"
+                  onClick={() => {
+                    handleClick(item);
+                  }}
+                >
+                  <h3>{item.title}</h3>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default Home;
